d223e8363ca5d9aec7f0f6ee4715517279e7fb09
max-powell <github email address>
2019-06-08 15:57:30 +0100

    Done.

--- src/containers/Quotes.js
+++ src/containers/Quotes.js
@@ -5,6 +5,9 @@ import QuoteCard from '../components/QuoteCard';
 class Quotes extends Component {
 
   render() {
+
+    const { quotes } = this.props;
+
     return (
       <div>
         <hr />
@@ -15,11 +18,7 @@ class Quotes extends Component {
         <div className="container">
           <div className="row">
             <div className="col-md-4">
-              {/*
-                TODO:
-
-                Render Quotes With QuoteCard component and pass down callback props for removing, upvoting and downvoting quotes
-               */}
+              {quotes.map(q => <QuoteCard key={q.id} quote={q} />)}
             </div>
           </div>
         </div>
@@ -28,5 +27,4 @@ class Quotes extends Component {
   }
 }
 
-//add arguments to connect as needed
-export default connect()(Quotes);
+export default connect(state => ({quotes: state.quotes}))(Quotes);
